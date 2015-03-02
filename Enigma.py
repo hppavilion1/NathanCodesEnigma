927265764406850578fd611a47d5adb621e2818c
hppavilion1 <nathantronnes@gmail.com>
2015-03-01 17:51:48 -0900

    Rotor and Turnover Dicts
    Episode 3 commit 1

---
+++ Enigma.py
@@ -0,0 +1,7 @@
+A = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+listname = ['value1', 'value2', 'value3']
+Rotors = {'key1':'value1',
+          'key2':'value2',
+          'key3':'value3'
+    }
+print(Rotors['key1'])
